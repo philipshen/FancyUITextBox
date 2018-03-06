81e90d18922ad480eaa6aeb915fe68745510a7fc
Philip Shen <philipshen13@gmail.com>
2018-03-06 22:12:31 +0800

    Initial Commit

---
+++ FancyTextBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FancyTextBox
+//
+//  Created by Philip Shen on 3/6/18.
+//  Copyright © 2018 Philip Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
