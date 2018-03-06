48b165e5084230a686ea80037ec56b6297e9e660
Philip Shen <philipshen13@gmail.com>
2018-03-07 00:07:48 +0800

    Initial commit

---
+++ FancyTextBox/TextBox/TextBoxView.swift
@@ -0,0 +1,21 @@
+//
+//  TextBoxView.swift
+//  FancyTextBox
+//
+//  Created by Philip Shen on 3/6/18.
+//  Copyright © 2018 Philip Shen. All rights reserved.
+//
+
+import UIKit
+
+class TextBoxView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
